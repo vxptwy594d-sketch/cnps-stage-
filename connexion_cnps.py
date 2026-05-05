d4cbc5f91203bc9fffb133e7a71fcc406449b72a
vxptwy594d-sketch <hanieltchapmeni@icloud.com>
2026-05-05 12:25:36 +0100

    Remplace ConnexionODBC par DeclarationCNPS pour la gestion de la connexion à la base de données

---
+++ connexion_cnps.py
@@ -0,0 +1,19 @@
+
+
+
+class DeclarationCNPS:
+    def __init__(self, server, database, username, password):
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+    
+    def connecter(self):
+        ...
+    
+
+    def leecture_des_donnees(self):
+        ...
+    
+    def creation_du_fichier_de_declaration(self):
+        ...
--- declare_cnps.py
+++ declare_cnps.py
@@ -1,21 +1,11 @@
-from connexion_cnps import ConnexionODBC
+from connexion_cnps import DeclarationCNPS
 
 # Créer et connecter
-db = ConnexionODBC(
+db = DeclarationCNPS(
     server="MON_SERVEUR",
     database="PAIE_CNPS",
     username="mon_utilisateur",
     password="mon_mot_de_passe"
 )
 
-db.connecter()
-cursor = db.get_cursor()
 
-# Votre requête
-cursor.execute("SELECT * FROM employes WHERE actif = 1")
-
-# Parcourir les résultats
-for row in cursor.fetchall():
-    print(row)
-
-db.deconnecter()
